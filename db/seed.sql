d0410535492cef00b82d7d5c88f3cb4cb2ac16cf
Benicio Lopez <benicio.lopez@gmail.com>
2021-09-19 14:54:13 -0500

    fixed syntax error seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -13,11 +13,11 @@ VALUES
 ('Salesperson', 1, 70000),
 ('Accountant', 2, 75000),
 ('Software Engineer', 3, 90000),
-('Lawyer', 4, 100000)
+('Lawyer', 4, 100000);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
 ('John', 'Smith', 1, NULL),
 ('Erick', 'Johnson', 2, NULL),
 ('Tom', 'Allen', 3, NULL),
-('Jared', 'Garza', 4, NULL)
+('Jared', 'Garza', 4, NULL);
